24ada56a77cd1c9c35affaf6b5737c5eefd23d7c
Vijay Kumar Gattikoppula <gtkvijaykumar@gmail.com>
2024-06-21 13:48:58 +0530

    blogPost flatList Changes

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,12 +1,22 @@
 import React,{useContext} from "react";
-import { Text,View,StyleSheet } from "react-native";
+import { Text,View,StyleSheet, FlatList } from "react-native";
 import BlogContext from "../context/blogContext";
 
 const indexScreen = () => {
 const value = useContext(BlogContext);
     return( <View>
         <Text>This is Index Screen</Text>
-        <Text>{value}</Text>
+        <FlatList 
+            data={value}
+            keyExtractor= { item => item.title}
+            renderItem={({item}) => 
+                {
+                    return <Text>
+                        {item.title}
+                    </Text>
+                }
+               }
+        />
     </View>);
 
 };
